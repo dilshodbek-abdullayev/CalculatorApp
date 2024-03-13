8fab5f26e4d85d88a898e5e0e6ac342e242ef3d5
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-03-13 13:57:39 +0500

    UnitTest Project qo'shildi

---
+++ CalculatorConsole.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CalculatorConsole.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+        [Fact]
+        public void Test2()
+        {
+
+        }
+    }
+}
