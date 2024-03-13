e909d287c395c30cad597d1a6f191685e927add1
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-03-13 13:51:48 +0500

    Add project files.

---
+++ CalculatorApp.Console/Matematics.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp.Console
+{
+    public static class Matematics
+    {
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public static double Subtrak(double x, double y)
+        {
+            return x - y;
+        }
+
+    }
+}
---
+++ CalculatorApp.Console/Program.cs
@@ -0,0 +1,8 @@
+﻿using CalculatorApp.Console;
+
+double a = 2;
+double b = 3;
+
+var result =Matematics.Add(a, b);
+
+Console.WriteLine(result);
